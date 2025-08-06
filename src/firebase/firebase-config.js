4eb370bdcfbad65c1eafc98bfe3382508050dbbd
jason <jaml08@hotmail.com>
2025-08-05 21:53:53 -0500

    .env

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCksS2camzEwStgY4oCKpTv-nvuFNPJzcM",
-  authDomain: "shop-auth-ceead.firebaseapp.com",
-  projectId: "shop-auth-ceead",
-  storageBucket: "shop-auth-ceead.firebasestorage.app",
-  messagingSenderId: "392757618745",
-  appId: "1:392757618745:web:c5613f20b29afec7ad45e8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
